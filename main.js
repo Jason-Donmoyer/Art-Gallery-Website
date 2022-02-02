5822999348061dc38c2e3c5cf46f9437662f356f
Jason-Donmoyer <j.donmoyer523@gmail.com>
2022-02-02 17:28:01 -0500

    HTML, CSS and JS: -HTML: --added containers for footer styling --markup for location.html -CSS: --styles for location.html started --fixed issue with Twitter icon repeating in background -JS: --functionality for buttons to nav between pages

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const ourLocationBtn = document.getElementById('our-location-btn');
+const backToHomeBtn = document.getElementById('back-to-home-btn');
+
+if (ourLocationBtn) {
+  ourLocationBtn.addEventListener('click', () => {
+    openTab('location.html');
+  });
+}
 
 
+if (backToHomeBtn) {
+  backToHomeBtn.addEventListener('click', () => {
+    openTab('index.html');
+  });
+}
 
-ourLocationBtn.addEventListener('click', () => {
-  openTab('location.html');
-});
 
 
 
